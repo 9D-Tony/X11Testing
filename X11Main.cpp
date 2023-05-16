d620498b472010218b6c372860ec270dda16ea32
Tony Demot <tonydemot@gmail.com>
2023-05-16 23:59:34 +0100

    Update X11Main, Add x11Window

--- X11Main.cpp
+++ X11Main.cpp
@@ -10,22 +10,8 @@
 #endif
 
 #include<stdint.h>
+#include "x11window.cpp"
 
-Display *dis;
-int screen;
-Window win;
-GC gc;
-XFontStruct* myFont;
-
-void close_x() {
-/* it is good programming practice to return system resources to the 
-   system...
-*/
-	XFreeGC(dis, gc);
-	XDestroyWindow(dis,win);
-	XCloseDisplay(dis);	
-	exit(0);				
-}
 
 unsigned long RGB(int r, int g, int b)
 {
@@ -42,17 +28,21 @@ long getTimeInMicroseconds()
 int main()
 {
     unsigned long black,white, red, blue;
+    
     uint32_t  FPS = 30;
 
     dis = XOpenDisplay((char *)0);
    	screen = DefaultScreen(dis);
+    
 	black = BlackPixel(dis,screen),	/* get color black */
 	white = WhitePixel(dis, screen);  /* get color white */
+    
 	red = RGB(255,0,0);
 	blue = RGB(0,0,255);
 	
     int windowWidth = 800; 
     int windowHeight = 600;
+    
     win = XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,	windowWidth, windowHeight, 0, white, black);
 
 	XSetStandardProperties(dis,win,"My Window","HI!",None,NULL,0,NULL);
@@ -64,13 +54,12 @@ int main()
 	/* here is another routine to set the foreground and background
 	   colors _currently_ in use in the window.
 	*/
+    
 	XSetBackground(dis,gc,white);
 	XSetForeground(dis,gc,black);
 
 	/* clear the window and bring it on top of the other windows */
 	XClearWindow(dis, win);
-    
-     //XSetWindowBackgroundPixmap(dis,win,None);
     
 	XMapRaised(dis, win);
 	
@@ -115,50 +104,53 @@ int main()
         
         //try sending event everyframe.
         XSendEvent(dis,win,false,ExposureMask,&ev);
-		XNextEvent(dis, &event);
         
-        unsigned long end = getTimeInMicroseconds();
-        
-		if (event.type==Expose && event.xexpose.count==0) {
-		/* the window was exposed redraw it! */
-			//printf("Exposed window, clearing!\n");
-
+        while(XPending(dis))
+        {
+            XNextEvent(dis, &event);
             
-        }
+            if (event.type==Expose && event.xexpose.count==0) {
+			//printf("Exposed window, clearing!\n")
+            
+                
+            }
 		
-        if (event.type==KeyPress &&
-		    XLookupString(&event.xkey,text,255,&key,0)==1) {
+        if (event.type==KeyPress && XLookupString(&event.xkey,text,255,&key,0)==1) {
 		/* use the XLookupString routine to convert the invent
 		   KeyPress data into regular text.  Weird but necessary...
 		*/
 			if (text[0]=='q') {
 				printf("Closing Window!\n");
-				close_x();
+				CloseX(dis,win,gc);
 			}
 
 			if (text[0]=='s') {
 				XClearWindow(dis,win);
 				printf("The window has been cleared!\n");
-			}
+                }
 
 			printf("You pressed the %c key!\n",text[0]);
-		}
-		
-		if (event.type==ButtonPress) {
+            
+            }
+            
+            if (event.type==ButtonPress) {
 		/* tell where the mouse Button was Pressed */
 			printf("You pressed a button at (%i,%i)\n",
 			event.xbutton.x,event.xbutton.y);
             
             //check if mouse click was in a button
-            
-            
-            
-		}
+            }
 
-        if (event.xclient.data.l[0] == wmDeleteMessage)
-		{
-            running = false;
-		}
+            if (event.xclient.data.l[0] == wmDeleteMessage)
+            {
+                running = false;
+            }
+            
+        }
+        
+        unsigned long end = getTimeInMicroseconds();
+		XSetForeground(dis,gc,white);
+		XDrawString(dis,win,gc,50,50,"hello",strlen("hello"));
 		
 		XSetForeground(dis,gc,blue);
         XDrawRectangle(dis,win,gc,x,y,20,20);
@@ -168,13 +160,13 @@ int main()
 		lastRepaint = getTimeInMicroseconds();
         // see if this works on tinkerboard
         
-		XSync(dis,true); //at moment sync is disposing of all other messages, 
+		XSync(dis,false);
 		//TODO: need a better way    
         usleep(1000*1000 / FPS);
 	}
 
 	//clearup window stuff
-	close_x();
+    CloseX(dis,win,gc);
     
     
     return 0;
